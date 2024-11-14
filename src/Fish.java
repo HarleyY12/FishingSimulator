9d85666b0da7cae4579b0d53180b21d6f72c5390
HarleyY12 <hyang2496@gmail.com>
2024-11-13 19:38:04 -0500

    Update Fish.java
    I added a variable points that assigns each fish a point value and a method getPoints to return the number of points.

--- src/Fish.java
+++ src/Fish.java
@@ -2,11 +2,13 @@ public class Fish {
     private final String name;
     private final double weight;
     private final double length;
+    private final int points;
 
-    public Fish (String name , double weight, double length){
+    public Fish (String name , double weight, double length, int points){
         this.name = name;
         this.weight = weight;
         this.length = length;
+        this.points = points
     }
     public String getName(){
       return name;
@@ -17,4 +19,9 @@ public class Fish {
     public double getLength(){
         return length;
     }
+    public int getPoints(){
+        return points;
+    }
+
+
 }
