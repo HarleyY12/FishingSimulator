6ef0d785d0c131ce262e619280363f551562aa9c
HarleyY12 <hyang2496@gmail.com>
2024-11-14 19:46:50 -0500

    Update FishSimulator.java
    I added methods that returned the total fishes and points earned. I also added a method that informs the user how much time they have to catch a fish and the random process of whether or not a fish  appears.

--- src/FishSimulator.java
+++ src/FishSimulator.java
@@ -1,36 +1,40 @@
 
-
 import java.util.List;
+import java.util.Random;
 public class FishSimulator {
-   private List<Fish> fishList;
-   private int numfishCaught;
-   private int score;
-   private boolean isCaught;
-   private long timeInMilliseconds;
-   private long startTime;
-   
-   public FishSimulator( List<Fish>fishList,long timeInMilliseconds){
-      this.fishList = fishList;
-      this.numFishCaught = 0;
-      this.score = 0;
-      this.isCaught = false;
-      this.timeInMilliseconds = timeInMilliseconds;
-   }
-   public String fishingTime(){
-      return "You have " + (timeInMilliseconds/1000) + " seconds to fish !";
-   }
-   public int castLine(){
-      if (System.currentTimeMillis() > timeInMilliseconds){
-         return "Time's up!"
-      }
-      
+    private List<Fish> fishList;
+    private int numFishCaught;
+    private int numfishCaught;
+    private int score;
+    private long timeInMilliseconds;
+
+    public FishSimulator( List<Fish>fishList,long timeInMilliseconds){
+        this.fishList = fishList;
+        this.numFishCaught = 0;
+        this.score = 0;
+        this.timeInMilliseconds = timeInMilliseconds;
+    }
+    public int getScore() {
+        return score;
+    }
+    public int getNumFishCaught() {
+        return numFishCaught;
+    }
+    public String startFishing(){
+        return "You have " + (timeInMilliseconds/1000) + " seconds to fish !";
+    }
+    public String castLine() {
+        long startTime = System.currentTimeMillis();
+        if (System.currentTimeMillis() - startTime > timeInMilliseconds) {
+            return "Get ready!";
+        }
+        if (Math.random() > 0.5 && numfishCaught < 50) {
+            Fish caughtFish = fishList.get(new Random().nextInt(fishList.size()));
+            return "A" + caughtFish.getName() + " has been found! Press e to fish it.";
+        }
+        return "no fish have been found";
+    }
 
 
-   }
 
-}
-
-
-
-
-}
+    }
