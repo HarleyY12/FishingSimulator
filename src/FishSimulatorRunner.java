9c874cfd29dd4e226ca9fb9fd46536cf02e6b518
HarleyY12 <hyang2496@gmail.com>
2024-11-14 19:44:48 -0500

    Update FishSimulatorRunner.java
    I imported arrays so I could use the initialized fish objects as arguments for the fish simulator class.

--- src/FishSimulatorRunner.java
+++ src/FishSimulatorRunner.java
@@ -1,23 +1,21 @@
-import java.util.List;
+
+import java.util.Arrays;
 import java.util.Scanner;
-import java.util.ArrayList;
+
 
 public class FishSimulatorRunner {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        List<String>fishList = new ArrayList<>();
-        Fish fish1 = new Fish("goldfish",0.1,0.05);
-        Fish fish2 = new Fish("catfish",90,5);
-        Fish fish3 = new Fish("salmon",20,4);
+        Fish fish1 = new Fish("goldfish",0.1,0.5,1);
+        Fish fish2 = new Fish("bass",30,60,5);
+        Fish fish3 = new Fish("salmon",50,80,8);
+        FishSimulator simulator = new FishSimulator(Arrays.asList(fish1,fish2,fish3),5000);
+
         System.out.println("Welcome to fishing simulator.");
         System.out.println("You will have a time limit to press 'e' to 'fish'.");
         System.out.println("You need 100 points to win.");
         System.out.println();
-        String enteredKey;
-        enteredKey = s.next();
-        if (enteredKey.toLowerCase() == "e"){
-            System.out.println("you caught the fish!");
-        }
+        
 
 
     }
