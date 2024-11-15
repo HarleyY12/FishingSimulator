753fae2f14a7259839316e91616d8d52eb4654d6
HarleyY12 <hyang2496@gmail.com>
2024-11-15 10:48:50 -0500

    Update FishSimulator.java
    I started working on the fishing methods that determine whether or not a fish was found and if the number of fishing attempts was smaller than the max amount. I also tried to create a catch fish method that takes user input but I am not sure how to implement it within the object class and carry over to the runner class.

--- src/FishSimulator.java
+++ src/FishSimulator.java
@@ -3,38 +3,56 @@ import java.util.List;
 import java.util.Random;
 public class FishSimulator {
     private List<Fish> fishList;
-    private int numFishCaught;
-    private int numfishCaught;
-    private int score;
+    private int numFishCaught = 0;
+    private int score = 0;
+    private int numFishingAttempts = 0;
     private long timeInMilliseconds;
+    boolean fishFound = false;
 
-    public FishSimulator( List<Fish>fishList,long timeInMilliseconds){
+    public FishSimulator(List<Fish> fishList, long timeInMilliseconds) {
         this.fishList = fishList;
-        this.numFishCaught = 0;
-        this.score = 0;
         this.timeInMilliseconds = timeInMilliseconds;
     }
+
     public int getScore() {
         return score;
     }
+
     public int getNumFishCaught() {
         return numFishCaught;
     }
-    public String startFishing(){
-        return "You have " + (timeInMilliseconds/1000) + " seconds to fish !";
+
+    public String startFishing() {
+        return "You have " + (timeInMilliseconds / 1000) + " seconds to get ready to fish !";
     }
+
     public String castLine() {
-        long startTime = System.currentTimeMillis();
-        if (System.currentTimeMillis() - startTime > timeInMilliseconds) {
+
+        if (System.currentTimeMillis() > timeInMilliseconds) {
             return "Get ready!";
         }
-        if (Math.random() > 0.5 && numfishCaught < 50) {
+        if (Math.random() > 0.9 && numFishingAttempts < 21) {
             Fish caughtFish = fishList.get(new Random().nextInt(fishList.size()));
-            return "A" + caughtFish.getName() + " has been found! Press e to fish it.";
+            fishFound = true;
+            return "A " + caughtFish.getName() + " has been found! Press e to fish it.";
+        } else {
+            fishFound = false;
+            return "no fish have been found";
         }
-        return "no fish have been found";
     }
 
+        public String catchFish() {
+            while (numFishingAttempts != 21) {
+
+                Fish caughtFish = fishList.get(new Random().nextInt(fishList.size()));
+                numFishCaught = numFishCaught + 1;
+                numFishingAttempts = numFishingAttempts + 1;
+                score = score + caughtFish.getPoints();
+                return "you have caught a " + caughtFish.getName() + " with " + caughtFish.getWeight() + " lbs " + caughtFish.getLength() + " in";
+                }
+                return "you have fished the max amount of times";
+            }
+        }
 
 
-    }
+
