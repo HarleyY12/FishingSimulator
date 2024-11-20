50a092bb0594953d152f0a3c5247185f657c66ad
HarleyY12 <hyang2496@gmail.com>
2024-11-20 10:42:01 -0500

    Update FishSimulatorRunner.java
    I am trying to implement a system that checks for user confirmation to start and keeps asking until it is received.

--- src/FishSimulatorRunner.java
+++ src/FishSimulatorRunner.java
@@ -7,15 +7,16 @@ public class FishSimulatorRunner {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         Fish fish1 = new Fish("goldfish",0.1,0.5,1);
-        Fish fish2 = new Fish("bass",30,60,5);
-        Fish fish3 = new Fish("salmon",50,80,8);
-        FishSimulator simulator = new FishSimulator(Arrays.asList(fish1,fish2,fish3),5000);
+        Fish fish2 = new Fish("bass",30,60,3);
+        Fish fish3 = new Fish("salmon",50,80,5);
+        FishSimulator simulator = new FishSimulator(Arrays.asList(fish1,fish2,fish3),100000);
 
         System.out.println("Welcome to fishing simulator.");
         System.out.println("You will have a time limit for each fishing attempt.");
         System.out.println("You need 50 points to win.You will have a maximum of 20 fishing attempts.");
-        System.out.println(simulator.startFishing());
+        System.out.println("Type 'start' to begin");
 
+        System.out.println(simulator.startFishing());
         while (!simulator.gameOver()){
             String castResults = simulator.castLine();
             System.out.println(castResults);
@@ -28,10 +29,9 @@ public class FishSimulatorRunner {
                 System.out.println("Recasting line...");
             }
         }
-
-        if (simulator.getScore() >= 50 && simulator.getNumFishingAttempts() < 20){
+        if (simulator.getScore() >= 40 && simulator.getNumFishingAttempts() < 15){
             System.out.println("You win!");
-        }else if (simulator.getScore()<50  || simulator.getNumFishingAttempts() > 20){
+        }else if (simulator.getScore() < 40  || simulator.getNumFishingAttempts() > 15) {
             System.out.println("You lost. Try again next time.");
         }
     }
