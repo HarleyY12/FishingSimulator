6dd8b35856c48d87a4640311ce1fac79312f8d5a
HarleyY12 <hyang2496@gmail.com>
2024-11-21 19:44:28 -0500

    Update Fish.java
    I added comments to the runner class

--- src/Fish.java
+++ src/Fish.java
@@ -1,17 +1,21 @@
-public class Fish {
-    private final String name;
-    private final double weight;
-    private final double length;
-    private final int points;
 
+//Fish Class represents a fish with different properties
+public class Fish {
+    private final String name;   //Name of fish
+    private final double weight;  //Weight of fish in pounds
+    private final double length;  //Length of fish in inches
+    private final int points;  //Number of points a fish is worth
+
+    //Constructor to initialize fish object
     public Fish (String name , double weight, double length, int points){
         this.name = name;
         this.weight = weight;
         this.length = length;
-        this.points = points
+        this.points = points;
     }
+    //Get methods to access separate fish properties
     public String getName(){
-      return name;
+        return name;
     }
     public double getWeight(){
         return weight;
@@ -22,6 +26,4 @@ public class Fish {
     public int getPoints(){
         return points;
     }
-
-
 }
