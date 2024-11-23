7644c56d58f30483792812682960911003145e33
HarleyY12 <hyang2496@gmail.com>
2024-11-23 09:21:46 -0500

    Update FishSimulatorRunner.java
    I added more fish objects

--- src/FishSimulatorRunner.java
+++ src/FishSimulatorRunner.java
@@ -8,15 +8,17 @@ public class FishSimulatorRunner {
         Scanner s = new Scanner(System.in);
         //Initialize fish objects
         Fish fish1 = new Fish("goldfish", 0.1, 0.5, 1);
-        Fish fish2 = new Fish("bass", 30, 60, 3);
-        Fish fish3 = new Fish("salmon", 50, 80, 5);
+        Fish fish2 = new Fish("bass", 10, 20, 2);
+        Fish fish3 = new Fish("salmon", 15, 30, 5);
+        Fish fish4 = new Fish("carp",8.5,25,3);
+        Fish fish5 = new Fish("tuna",25.5,60,8);
         //Create new instance of FishSimulator
-        FishSimulator simulator = new FishSimulator(Arrays.asList(fish1, fish2, fish3), 3500);
+        FishSimulator simulator = new FishSimulator(Arrays.asList(fish1, fish2, fish3, fish4, fish5), 40000);
 
         //Introduce user to program and how to begin
         System.out.println("Welcome to fishing simulator.");
         System.out.println("You will have a time limit for each fishing attempt.");
-        System.out.println("You need 50 points to win.You will have a maximum of 20 fishing attempts.");
+        System.out.println("You need 40 points to win.You will have a maximum of 20 fishing attempts.");
         System.out.println("Type 'start' to begin");
         //Wait for user to type 'start'
         String reply = "";
@@ -32,7 +34,7 @@ public class FishSimulatorRunner {
             String castResults = simulator.castLine();
             System.out.println(castResults);
             if (!castResults.contains("No fish")) {  //If a fish is found, ask user to enter trigger
-                System.out.println("Enter '" + simulator.getFishingCues() + "' to fish");
+                System.out.println("Enter '" + simulator.getFishingCues() + "' to fish.");
                 String userInput = s.next();
                 String catchResults = simulator.catchFish(userInput);
                 System.out.println(catchResults);  //Print whether the fish was caught
