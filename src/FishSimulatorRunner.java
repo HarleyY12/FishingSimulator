9b67810562839d02d1c33b84bece232832aaf659
HarleyY12 <hyang2496@gmail.com>
2024-11-23 09:06:57 -0500

    Update FishSimulatorRunner.java
    I added an extra }

--- src/FishSimulatorRunner.java
+++ src/FishSimulatorRunner.java
@@ -1,4 +1,3 @@
-
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -8,11 +7,11 @@ public class FishSimulatorRunner {
         //Scanner for user input
         Scanner s = new Scanner(System.in);
         //Initialize fish objects
-        Fish fish1 = new Fish("goldfish",0.1,0.5,1);
-        Fish fish2 = new Fish("bass",30,60,3);
-        Fish fish3 = new Fish("salmon",50,80,5);
+        Fish fish1 = new Fish("goldfish", 0.1, 0.5, 1);
+        Fish fish2 = new Fish("bass", 30, 60, 3);
+        Fish fish3 = new Fish("salmon", 50, 80, 5);
         //Create new instance of FishSimulator
-        FishSimulator simulator = new FishSimulator(Arrays.asList(fish1,fish2,fish3),3500);
+        FishSimulator simulator = new FishSimulator(Arrays.asList(fish1, fish2, fish3), 3500);
 
         //Introduce user to program and how to begin
         System.out.println("Welcome to fishing simulator.");
@@ -21,7 +20,7 @@ public class FishSimulatorRunner {
         System.out.println("Type 'start' to begin");
         //Wait for user to type 'start'
         String reply = "";
-        while (!(reply.equalsIgnoreCase("start"))){
+        while (!(reply.equalsIgnoreCase("start"))) {
             System.out.println("Waiting to start.");
             reply = s.next();
         }
@@ -29,23 +28,24 @@ public class FishSimulatorRunner {
         //Start the fishing simulator
         System.out.println(simulator.startFishing());
         //Loop until game over
-        while (!simulator.gameOver()){
+        while (!simulator.gameOver()) {
             String castResults = simulator.castLine();
             System.out.println(castResults);
-            if (!castResults.contains("No fish")){  //If a fish is found, ask user to enter trigger
+            if (!castResults.contains("No fish")) {  //If a fish is found, ask user to enter trigger
                 System.out.println("Enter '" + simulator.getFishingCues() + "' to fish");
                 String userInput = s.next();
                 String catchResults = simulator.catchFish(userInput);
                 System.out.println(catchResults);  //Print whether the fish was caught
-            }else{
+            } else {
                 System.out.println("Recasting line...");
             }
         }
 
         //Determine game outcome based on score and number of fishing attempts
-        if (simulator.getScore() >= 40 && simulator.getNumFishingAttempts() <= 20){
+        if (simulator.getScore() >= 40 && simulator.getNumFishingAttempts() <= 20) {
             System.out.println("You win!");
-        }else if (simulator.getScore() < 40  || simulator.getNumFishingAttempts() > 20) {
+        } else if (simulator.getScore() < 40 || simulator.getNumFishingAttempts() > 20) {
             System.out.println("You lost. Try again next time.");
         }
     }
+}
